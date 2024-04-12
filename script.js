0474dcba318ab1cbccebc1a1a997f12b1b92abb4
tibiolo <161530252+tibiolo@users.noreply.github.com>
2024-04-12 12:16:20 +0100

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// Open/Close - Login Popup
+
+function openForm() {
+    document.getElementById("login-popup").style.display = "grid";
+  }
+  
+function closeForm() {
+    document.getElementById("login-popup").style.display = "none";
+  }
+
