59dbea2f2561518e7da4ba97b84fea3867ad9b60
tibiolo <161530252+tibiolo@users.noreply.github.com>
2024-04-22 16:44:50 +0100

    .

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 // Open/Close - Login Popup
 
 function openForm() {
-    document.getElementById("login-popup").style.display = "grid";
-  }
-  
-function closeForm() {
-    document.getElementById("login-popup").style.display = "none";
-  }
+  document.getElementById("login-popup").style.display = "grid";
+}
 
+function closeForm() {
+  document.getElementById("login-popup").style.display = "none";
+}
+
+// Selection - Button Animation
+
+//$("#selection-banner").hover(function () {
+//  $(".shop-now-button").css("opacity", "100%");
+//});
+
+$("#selection-banner1").hover(function () {
+    // over
+    $(".shop-now-button1").animate({
+      opacity: '1',
+    }, "fast");
+  }, function () {
+    // out
+    $(".shop-now-button1").animate({
+      opacity: '0',
+    }, "fast");
+  }
+);
+
+$("#selection-banner2").hover(function () {
+  // over
+  $(".shop-now-button2").animate({
+    opacity: '1',
+  }, "fast");
+}, function () {
+  // out
+  $(".shop-now-button2").animate({
+    opacity: '0',
+  }, "fast");
+}
+);
